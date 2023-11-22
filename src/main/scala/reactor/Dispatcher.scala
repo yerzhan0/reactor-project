74968b5ca7505d53f49ee4f1f8241ebabc6752bf
Nikolai Semin <nikolaisemin01@gmail.com>
2023-11-22 11:09:03 +0200

    WiP

--- src/main/scala/reactor/Dispatcher.scala
+++ src/main/scala/reactor/Dispatcher.scala
@@ -1,6 +1,6 @@
 // group 1
 // 123456 Firstname Lastname
-// 654321 Firstname Lastname
+// 936323 Nikolai Semin
 
 package reactor
 
@@ -9,22 +9,32 @@ import reactor.api.{Event, EventHandler}
 final class Dispatcher(private val queueLength: Int = 10) {
   require(queueLength > 0)
 
+  private val queue = new BlockingEventQueue[Event[_]](queueLength)
+
   @throws[InterruptedException]
-  def handleEvents(): Unit = ???
+  def handleEvents(): Unit = {
+      val mainHandlindThread = new WorkerThread
+      mainHandlindThread.run()
+  }
 
-  def addHandler[T](h: EventHandler[T]): Unit = ???
+  def addHandler[T](h: EventHandler[T]): Unit = {
 
-  def removeHandler[T](h: EventHandler[T]): Unit = ???
+  }
+
+  def removeHandler[T](h: EventHandler[T]): Unit = {
+    
+  }
 
 }
 
-/*
-// Hint:
-final class WorkerThread[T](???) extends Thread {
 
- override def run(): Unit = ???
+// Hint:
+final class WorkerThread[T]() extends Thread {
+
+ override def run(): Unit = {
+  // do events habdling in that thread
+ }
 
  def cancelThread(): Unit = ???
 
 }
-*/
