b674a7fed3d65a71b215fbbd66245b93c5bcaf76
Yerzhan Zhamashev <34035490+yerzhan0@users.noreply.github.com>
2023-12-02 20:27:25 +0200

    document and refactor

--- src/main/scala/reactor/Dispatcher.scala
+++ src/main/scala/reactor/Dispatcher.scala
@@ -1,60 +1,103 @@
-// group 17
-// 906984 Yerzhan Zhamashev
-// 936323 Nikolai Semin
-
 package reactor
 
 import reactor.api.{Event, EventHandler, Handle}
-import scala.collection.mutable.Set
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{Set}
+import scala.collection.immutable.{List}
 
+/**
+ * Dispatcher class is responsible for managing event handlers and dispatching events to them.
+ * 
+ * @param queueLength The maximum length of the event queue. Must be greater than 0.
+ */
 final class Dispatcher(private val queueLength: Int = 10) {
-  require(queueLength > 0)
+  require(queueLength > 0, "Queue length must be greater than 0.")
 
   private val handlers: Set[EventHandler[Any]] = Set()
   private val eventsQueue = new BlockingEventQueue[Any](queueLength)
-  private val threadList: ListBuffer[WorkerThread] = ListBuffer.empty
+  private var threadList: List[WorkerThread] = List.empty
 
+  /**
+   * Continuously handles events from the queue as long as there are handlers available.
+   * 
+   * @throws InterruptedException if the thread is interrupted.
+   */
   @throws[InterruptedException]
   def handleEvents(): Unit = {
     while (handlers.nonEmpty) {
       val event = eventsQueue.dequeue
-      val handler = event.getHandler
-      if (handlers.contains(handler)){
-        event.dispatch()
+      if (handlers.contains(event.getHandler)) {
+        event.dispatch() // Dispatch the event if its handler is present.
       }
     }
   }
 
+  /**
+   * Adds an event handler for the dispatcher to manage.
+   * 
+   * @param h The event handler to add.
+   * @tparam T The type of event data the handler is dealing with.
+   */
   def addHandler[T](h: EventHandler[T]): Unit = {
-    val thread = new WorkerThread(h.asInstanceOf[EventHandler[Any]])
-    threadList += thread
-    handlers += h.asInstanceOf[EventHandler[Any]]
-    thread.start()
+    if (handlers.add(h.asInstanceOf[EventHandler[Any]])) {
+      addAndStartHandlerWorker(h)
+    }
   }
 
+  /**
+   * Starts a new worker thread for an event handler.
+   * 
+   * @param h The event handler to add.
+   * @tparam T The type of event data the handler is dealing with.
+   */
+  private def addAndStartHandlerWorker[T](h: EventHandler[T]): Unit = {
+    val worker = new WorkerThread(h.asInstanceOf[EventHandler[Any]])
+    threadList = worker :: threadList
+    worker.start()
+  }
+
+  /**
+   * Removes an event handler and stops its associated worker thread.
+   * 
+   * @param h The event handler to remove.
+   * @tparam T The type of event data the handler is dealing with.
+   */
   def removeHandler[T](h: EventHandler[T]): Unit = {
-    val removed = handlers.remove(h.asInstanceOf[EventHandler[Any]])
-    if (removed) {
-      threadList.foreach { thread =>
+    if (handlers.remove(h.asInstanceOf[EventHandler[Any]])) {
+      // Stop and remove the threads associated with the handler from the list
+      threadList = threadList.filter(thread => {
         if (thread.getHandler == h) {
           thread.cancelThread()
+          false
+        } else {
+          true
         }
-      }
+      })
     }
   }
 
-  final class WorkerThread(private val handler: EventHandler[Any])
-      extends Thread {
+  /**
+   * Inner class representing a worker thread for each event handler.
+   */
+  private final class WorkerThread(private val handler: EventHandler[Any]) extends Thread {
     def getHandler: EventHandler[Any] = handler
+
     override def run(): Unit = {
-      val handle = handler.getHandle
-      var eventData = handle.read
-      while (eventData != null) {
-        eventsQueue.enqueue(Event(eventData, handler))
-        eventData = handle.read
+      try {
+        // Get the handle associated with the event handler a read the first event data
+        var eventData = handler.getHandle.read
+        while (eventData != null && !this.isInterrupted) {
+          eventsQueue.enqueue(Event(eventData, handler)) // Enqueue the event to dispatch it in correct order.
+          eventData = handler.getHandle.read // Continue reading event data.
+        }
+
+        // Enqueue the null event to signal the end of the event stream.
+        if (!this.isInterrupted) 
+          eventsQueue.enqueue(Event(eventData, handler))
+      } catch {
+        case e: InterruptedException => {
+          // Do nothing. The thread cannot work anymore (if eventsQueue.enqueue throws InterruptedException.)
+        }
       }
-      eventsQueue.enqueue(Event(eventData, handler))
     }
 
     def cancelThread(): Unit = {
